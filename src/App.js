6adb72a9a2e36737a25895b438097f61335e5c6e
Giuseppe A. Cannizzo <giuseppeacannizzojr@gmail.com>
2022-04-04 09:46:05 -0500

    Pie

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { AppRegistry, StyleSheet, Text, View, Button } from "react-native";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import PieChart from "./PieChart";
+
+export default class App extends React.Component {
+    state = {
+        data: [
+            { value: 12, color: "#2196F3" },
+            { value: 12, color: "#8BC34A" },
+            { value: 8, color: "#f44336" },
+            { value: 4, color: "#FF9800" }
+        ]
+    };
+
+    randomize = () => {
+        const { data } = this.state;
+
+        this.setState({
+            data: data.map(slice => ({
+                value: Math.random() + 0.1,
+                color: slice.color
+            }))
+        });
+    };
+
+    render() {
+        const { data } = this.state;
+
+        return (
+            <View style={styles.container}>
+                <PieChart
+                    style={styles.chart}
+                    strokeColor={"white"}
+                    strokeWidth={4}
+                    data={data}
+                />
+                <Button title="Press to randomize" onPress={this.randomize} />
+            </View>
+        );
+    }
 }
 
-export default App;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    chart: {
+        width: 300,
+        height: 300,
+        marginBottom: 20
+    }
+});
